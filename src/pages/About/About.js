6c47ee6d16ddc156b60f947700d3699a5ba5f689
Federico <fedeacos045@gmail.com>
2022-01-09 16:21:19 -0300

    added about information

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,12 +5,44 @@ const About = () => {
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         height: "90vh",
+        margin: "20px",
       }}
     >
-      <h1>About</h1>
+      <h1>SHORT STORY OF NC</h1>
+      <br />
+      <h3>
+        Our name, “NewCombin”, reminds us about the “new combination” between
+        seasoned executives and young professionals, characterized by a
+        disruptive mindset.
+      </h3>
+      <br />
+      <h3>
+        It is not a mere coincidence: Marcelo and Nicolás, NC’s founders, are a
+        father and a son gathered by the ambition of empowering businesses
+        through technology.
+      </h3>
+      <br />
+      <h3>
+        And if in everyday life, parents share their wisdom and children their
+        creativity, at NewCombin we combine those characteristics in our team to
+        bring unique and high-quality solutions to our clients.
+      </h3>
+      <br />
+      <h3>
+        NewCombin was born by the end of 2017 and, at the beginning of 2018, we
+        already were working as a start-up. At the end of that year, Ian joined
+        the founders, and they created the best team to make the business grow.
+      </h3>
+      <br />
+      <h3>
+        Each year since its foundation, NewCombin grew 300% and achieved its
+        presence in the American continent, helping businesses with software
+        development and digital transformation.
+      </h3>
     </div>
   );
 };
