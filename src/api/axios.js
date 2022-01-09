eeb6c70cb104e44f06c767923ddf094dd937c6ca
Federico <fedeacos045@gmail.com>
2022-01-09 08:44:26 -0300

    changed axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,41 @@
-/*import axios from "axios";
+import axios from "axios";
 
-axios.defaults.url = "http://localhost:8081/api/members";
-axios.defaults.headers.common = {
-  Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsIm5hbWUiOiJzYXJhaCIsImFkbWluIjpmYWxzZSwiaWF0IjoxNjQxNzAzNzE3LCJleHAiOjE2NDE3MDQ2MTd9.IN8HK23yZf7ingmI7cSEdL2LGjJyc9e7y_bqI9MBJO4`,
-};
-export default axios;
-*/
+const axiosConn = axios.create({
+  baseURL: `https://localhost:8081`,
+});
+
+export default axiosConn;
+
+// Request interceptor (Outgoing)
+axiosConn.interceptors.request.use(
+  function (config) {
+    console.log("Interceptor Request (Outgoing) ", config);
+
+    config.headers.API_KEY =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsIm5hbWUiOiJzYXJhaCIsImFkbWluIjpmYWxzZSwiaWF0IjoxNjQxNzI3NzE5LCJleHAiOjE2NDE3Mjg2MTl9.Ouade6TiSzLQRy4GVI65z62TtE-OlxErXWe_283zQ0M";
+
+    if (sessionStorage.getItem("jwt_token")) {
+      config.headers.Authorization = `Bearer ${sessionStorage.getItem(
+        "jwt_token"
+      )}`;
+    }
+    return config;
+  },
+  function (error) {
+    // Request error
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+axiosConn.interceptors.response.use(
+  function (response) {
+    // Response data
+    console.log("Interceptor Response (Incoming) ", response);
+
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
